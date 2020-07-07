ee77b767a48bbc0dcb28b89b9c713468ef3ad892
yudystriawan <yudistriawan@gmail.com>
2020-07-07 13:45:12 +0800

    fix some code

--- lib/infrastucture/core/config_reader.dart
+++ lib/infrastucture/core/config_reader.dart
@@ -2,23 +2,24 @@ import 'dart:convert';
 
 import 'package:flutter/services.dart';
 
+// ignore: avoid_classes_with_only_static_members
 class ConfigReader {
   static Map<String, dynamic> _config;
 
   static Future<void> initialize() async {
     final configString = await rootBundle.loadString('config/app_config.json');
-    _config = jsonDecode(configString);
+    _config = jsonDecode(configString) as Map<String, dynamic>;
   }
 
   static String getPaypalBaseUrl() {
-    return _config['paypalBaseUrl'];
+    return _config['paypalBaseUrl'] as String;
   }
 
   static String getPaypalClientId() {
-    return _config['paypalClientId'];
+    return _config['paypalClientId'] as String;
   }
 
   static String getPaypalSecret() {
-    return _config['paypalSecret'];
+    return _config['paypalSecret'] as String;
   }
 }
